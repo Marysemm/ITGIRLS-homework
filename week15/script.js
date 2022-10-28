e0469c6488d6852ab6849a5a9d192dda4880e0e4
Marysemm <a.mascha.m@yandex.ru>
2022-10-28 23:02:20 +0300

    works evryth exept checkbox

--- week15/script.js
+++ week15/script.js
@@ -4,7 +4,6 @@ function checkInputs() {
     let number = document.querySelector('#number');
     let password = document.querySelector('#password1');
     let passwordRepeat = document.querySelector('#password2');
-    let checkbox = document.querySelector('#agreements');
 
     document.getElementById('errorMessage').innerHTML = "";
 
@@ -32,10 +31,6 @@ function checkInputs() {
         document.getElementById('errorMessage').innerHTML += "Ваш пароль слишком короткий<br>";
     }
 
-    if (!checkbox.сhecked) {
-        document.getElementById('errorMessage').innerHTML += "Подтвердите согласие на обработку данных<br>";
-    }
-
     if (password.value !== passwordRepeat.value) {
         document.getElementById('errorMessage').innerHTML += "Ваши пароли не совпадают<br>";
     }
@@ -44,3 +39,14 @@ function checkInputs() {
         alert(`Добро пожаловать, ${userName.value}!`)
     }
 }
+
+function checkCheckbox() {
+    let checkbox = document.querySelector('#agreements');
+    document.getElementById('errorMessage').innerHTML = "";
+
+    if (checkbox.сhecked == false) {
+        document.getElementById('errorMessage').innerHTML += "Подтвердите согласие на обработку данных<br>";
+    }
+}
+
+document.querySelector("#logIn").addEventListener('click', checkInputs);
