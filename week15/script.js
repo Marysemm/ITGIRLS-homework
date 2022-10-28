7e5e53ae80be262fc89f26a007f83c3ebf6d9752
Marysemm <a.mascha.m@yandex.ru>
2022-10-28 21:47:12 +0300

    added input for name

--- week15/script.js
+++ week15/script.js
@@ -1,22 +1,16 @@
-function checkCheckbox() {
-    let checkbox = document.querySelector('#agreements');
-    document.getElementById('errorMessage').innerHTML = "";
-
-    if (checkbox.сhecked) {
-        document.getElementById('errorMessage').innerHTML += "Вы дали согласие на обработку данных<br>";
-    }
-    else {
-        document.getElementById('errorMessage').innerHTML += "Вы дали согласие на обработку данных<br>";
-    }
-}
-
 function checkInputs() {
+    let userName = document.querySelector('#userName');
     let email = document.querySelector('#email');
     let number = document.querySelector('#number');
     let password = document.querySelector('#password1');
     let passwordRepeat = document.querySelector('#password2');
+    let checkbox = document.querySelector('#agreements');
 
     document.getElementById('errorMessage').innerHTML = "";
+
+    if (userName.value == '') {
+        document.getElementById('errorMessage').innerHTML += "Поле имя не заполнено<br>";
+    }
 
     if (email.value == '') {
         document.getElementById('errorMessage').innerHTML += "Поле почта не заполнено<br>";
@@ -26,9 +20,8 @@ function checkInputs() {
         document.getElementById('errorMessage').innerHTML += "Поле почта заполнено не верно<br>";
     }
 
-
     if (number.value == '') {
-        document.getElementById('errorMessage').innerHTML += "Поле номер не заполнено<br>";
+        document.getElementById('errorMessage').innerHTML += "Поле номер телефона не заполнено<br>";
     }
 
     if (number.value.length < 11) {
@@ -39,11 +32,15 @@ function checkInputs() {
         document.getElementById('errorMessage').innerHTML += "Ваш пароль слишком короткий<br>";
     }
 
+    if (!checkbox.сhecked) {
+        document.getElementById('errorMessage').innerHTML += "Подтвердите согласие на обработку данных<br>";
+    }
+
     if (password.value !== passwordRepeat.value) {
         document.getElementById('errorMessage').innerHTML += "Ваши пароли не совпадают<br>";
     }
 
     else {
-        alert("Вы успешно зарегистрировались!")
+        alert(`Добро пожаловать, ${userName.value}!`)
     }
 }
