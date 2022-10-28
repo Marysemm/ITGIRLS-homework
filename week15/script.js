9fc11d78d4c65ba43ac2ba2b1c0a25426a46023c
Marysemm <a.mascha.m@yandex.ru>
2022-10-28 23:53:47 +0300

    checkbox still doesn't work

--- week15/script.js
+++ week15/script.js
@@ -4,6 +4,7 @@ function checkInputs() {
     let number = document.querySelector('#number');
     let password = document.querySelector('#password1');
     let passwordRepeat = document.querySelector('#password2');
+    let checkbox = document.querySelector('#agreements');
 
     document.getElementById('errorMessage').innerHTML = "";
 
@@ -34,15 +35,6 @@ function checkInputs() {
     if (password.value !== passwordRepeat.value) {
         document.getElementById('errorMessage').innerHTML += "Ваши пароли не совпадают<br>";
     }
-
-    else {
-        alert(`Добро пожаловать, ${userName.value}!`)
-    }
-}
-
-function checkCheckbox() {
-    let checkbox = document.querySelector('#agreements');
-    document.getElementById('errorMessage').innerHTML = "";
 
     if (checkbox.сhecked == false) {
         document.getElementById('errorMessage').innerHTML += "Подтвердите согласие на обработку данных<br>";
