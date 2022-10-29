e51571d76fec6a5df41e20ae620ac7699e6c14e9
Marysemm <a.mascha.m@yandex.ru>
2022-10-30 00:54:01 +0300

    added alert

--- week15/script.js
+++ week15/script.js
@@ -17,33 +17,35 @@ function checkInputs() {
         document.getElementById('errorMessage').innerHTML += "Поле имя не заполнено<br>";
     }
 
-    if (email.value == '') {
+    else if (email.value == '') {
         document.getElementById('errorMessage').innerHTML += "Поле почта не заполнено<br>";
     }
 
-    if (email.value.indexOf('@', 0) == -1) {
+    else if (email.value.indexOf('@', 0) == -1) {
         document.getElementById('errorMessage').innerHTML += "Поле почта заполнено не верно<br>";
     }
 
-    if (number.value == '') {
+    else if (number.value == '') {
         document.getElementById('errorMessage').innerHTML += "Поле номер телефона не заполнено<br>";
     }
 
-    if (number.value.length < 11) {
+    else if (number.value.length < 11) {
         document.getElementById('errorMessage').innerHTML += "Номер телефона слишком короткий<br>";
     }
 
-    if (password.value.length <= 6) {
+    else if (password.value.length <= 6) {
         document.getElementById('errorMessage').innerHTML += "Ваш пароль слишком короткий<br>";
     }
 
-    if (password.value !== passwordRepeat.value) {
+    else if (password.value !== passwordRepeat.value) {
         document.getElementById('errorMessage').innerHTML += "Ваши пароли не совпадают<br>";
     }
 
-    if (checkbox.сhecked == false) {
+    else if (checkbox.сhecked == false) {
         document.getElementById('errorMessage').innerHTML += "Подтвердите согласие на обработку данных<br>";
     }
+
+    else (alert(`Добро пожаловать, ${userName.value}!`));
 }
 
 document.querySelector("#logIn").addEventListener('click', checkInputs);
