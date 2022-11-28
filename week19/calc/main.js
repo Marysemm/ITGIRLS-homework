ba3c5ba3ed8645ef44ddea5cda50f4163a647a72
Marysemm <a.mascha.m@yandex.ru>
2022-11-28 13:22:43 +0300

    finished calc task

--- week19/calc/main.js
+++ week19/calc/main.js
@@ -1,61 +1,80 @@
-const buttonPlus = document.querySelector('buttonPlus');
-const buttonMinus = document.querySelector('buttonMinus');
-const buttonMultiply = document.querySelector('buttonMultiply');
-const buttonDivide = document.querySelector('buttonDivide');
-const clearBtn = document.querySelector('clear');
-
-let input1 = document.getElementById('number1');
-let input2 = document.getElementById('number2');
+const buttonPlus = document.querySelector('#buttonPlus');
+const buttonMinus = document.querySelector('#buttonMinus');
+const buttonMultiply = document.querySelector('#buttonMultiply');
+const buttonDivide = document.querySelector('#buttonDivide');
+const errorMessage = document.getElementById('errorMessage');
+let result = document.getElementById('results');
 
 class Calculator {
+    static getNumbers() {
+        let number1 = document.getElementById('number1').value;
+        let number2 = document.getElementById('number2').value;
+        return [number1, number2];
+    }
+
     static sumNumbers() {
-        let number1 = Number(input1.value);
-        let number2 = Number(input2.value);
-        let result = number1 + number2;
-        document.getElementById('results').value = result;
+        let [number1, number2] = Calculator.getNumbers();
+        if (number1 !== " " && number2 !== " ") {
+            return number1 + number2;
+        }
     }
 
     static minusNumbers() {
-        let number1 = Number(input1.value);
-        let number2 = Number(input2.value);
-        let result = number1 - number2;
-        document.getElementById('results').value = result;
+        let [number1, number2] = Calculator.getNumbers();
+        if (number1 !== " " && number2 !== " ") {
+            return number1 - number2;
+        }
     }
 
-    static multilyNumbers() {
-        let number1 = Number(input1.value);
-        let number2 = Number(input2.value);
-        let result = number1 * number2;
-        document.getElementById('results').value = result;
+    static multiplyNumbers() {
+        let [number1, number2] = Calculator.getNumbers();
+        if (number1 !== " " && number2 !== " ") {
+            return number1 * number2;
+        }
     }
 
     static divideNumbers() {
-        let number1 = Number(input1.value);
-        let number2 = Number(input2.value);
-        let result;
-        document.getElementById('errorMessage').innerHTML = "";
+        let [number1, number2] = Calculator.getNumbers();
         if (number2 == 0) {
-            document.getElementById('errorMessage').innerHTML += 'На ноль делить нельзя';
+            errorMessage.innerHTML = "На ноль делить нельзя!";
         }
-        else {
-            result = number1 / number2;
-            document.getElementById('results').value = result;
+        else if (number1 !== " " && number2 !== " ") {
+            errorMessage.innerHTML = "";
+            return number1 / number2;
         }
     }
+}
 
-    static clearAll() {
-        input1.value = '';
-        input2.value = '';
-        results.value = '';
+buttonPlus.addEventListener("click", () => {
+    if (Calculator.sumNumbers() !== undefined) {
+        result.value = Calculator.sumNumbers();
     }
+});
+
+buttonMinus.addEventListener("click", () => {
+    if (Calculator.minusNumbers() !== undefined) {
+        result.value = Calculator.minusNumbers();
+    }
+});
+
+buttonMultiply.addEventListener("click", () => {
+    if (Calculator.multiplyNumbers() !== undefined) {
+        result.value = Calculator.multiplyNumbers();
+    }
+});
+
+buttonDivide.addEventListener("click", () => {
+    if (Calculator.divideNumbers() !== undefined) {
+        result.value = Calculator.divideNumbers();
+    }
+});
+
+function clearAll() {
+    document.getElementById('number1').value = "";
+    document.getElementById('number2').value = "";
+    result.value = "";
 }
 
 function changeColor() {
     document.getElementById('calculator').style.backgroundColor = "gray";
 }
-
-buttonPlus.addEventListener('click', Calculator.sumNumbers);
-buttonMinus.addEventListener('click', Calculator.minusNumbers);
-buttonMultiply.addEventListener('click', Calculator.multilyNumbers);
-buttonDivide.addEventListener('click', Calculator.divideNumbers);
-clearBtn.addEventListener('click', Calculator.clearAll);
